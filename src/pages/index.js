760038cfd50da4c071377693dc0151bd80af8dee
Portia <plburton@gmail.com>
2019-11-30 12:55:30 +0100

    added additional columns

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,13 @@ class IndexComponent extends React.Component {
             <tr>
               <th>Blog Title</th>
               <th>Blog URL</th>
+              <th>Notes</th>
+              <th>Blog Topic Cover in Reverence Guide</th>
+              <th>Blog Topic Cover in Tutorial</th>
+              <th>Tags</th>
+              <th>Questions</th>
+              <th>Should This Be Turned into a Blog</th>
+              <th>Date Published</th>
             </tr>
           </thead>
           <tbody>
@@ -23,6 +30,13 @@ class IndexComponent extends React.Component {
               <tr key={`${row.node.value} ${i}`}>
                 <td>{row.node.Blog_post_title}</td>
                 <td>{row.node.Blog_post_URL}</td>
+                <td>{row.node.Notes}</td>
+                <td>{row.node.Blog_Topic_Covered_in_Reference_Guide}</td>
+                <td>{row.node._Blog_Topic_Covered_in_Tutorials}</td>
+                <td>{row.node._Tags}</td>
+                <td>{row.node.Questions}</td>
+                <td>{row.node.Should_be_Turned_into_a_Doc_}</td>
+                <td>{row.node.Date_Published}</td>
               </tr>
             ))}
           </tbody>
@@ -42,6 +56,13 @@ export const query = graphql`
         node {
           Blog_post_title
           Blog_post_URL
+          Notes
+          Questions
+          Blog_Topic_Covered_in_Reference_Guide
+          _Blog_Topic_Covered_in_Tutorials
+          Should_be_Turned_into_a_Doc_
+          _Tags
+          Date_Published
         }
       }
     }
